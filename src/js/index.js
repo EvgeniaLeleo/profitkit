05595c08be4d8218e03556d92d435c89546e2ca2
Evgenia Fedorova <leleoartist@gmail.com>
2023-02-12 05:55:06 +0300

    feat: implement adaptive layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,11 @@
 import { generateContent } from './generateContent';
-import { MEDIA_425 } from './constants';
 import '../css/style.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   window.app = {
     currentItems: [],
-    isMobile: false,
   };
 
   const screenContainer = document.querySelector('.screen-container');
   generateContent(screenContainer);
-
-  const mediaQuery = window.matchMedia(MEDIA_425);
-
-  const handleWidthChange = (e) => {
-    if (e.matches) {
-      window.app.isMobile = true;
-    }
-  };
-
-  mediaQuery.addListener(handleWidthChange);
-  handleWidthChange(mediaQuery);
 });
