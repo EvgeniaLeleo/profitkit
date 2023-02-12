18143894bda9396c69460c55b1bfb47fd507f0ac
Evgenia Fedorova <leleoartist@gmail.com>
2023-02-12 05:23:34 +0300

    feat: implement mobile version

--- src/js/constants.js
+++ src/js/constants.js
@@ -6,6 +6,8 @@ export const DATA = { popular: 'popular', recent: 'recent', forYou: 'forYou' };
 
 export const DATA_KEYS = [DATA.popular, DATA.recent, DATA.forYou];
 
+export const MEDIA_425 = '(max-width: 425px)';
+
 export const DIRECTION = {
   left: 'left',
   right: 'right',
